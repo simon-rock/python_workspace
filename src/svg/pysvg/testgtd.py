b2db114a084468feeb951a1b5972d26eec31326b
simon-rock <simon29rock@gmail.com>
2013-09-06 17:59:09 +0800

    pysvg test update pysvg info in my_python.txt

---
+++ src/svg/pysvg/testgtd.py
@@ -0,0 +1,30 @@
+'''
+Created on 11.11.2010
+
+@author: kerim
+'''
+from pysvg import parser
+from pysvg.structure import svg
+from pysvg.shape import rect
+from pysvg.core import TextContent 
+
+def main():
+    anSVG = parser.parse('./sourceimages/gtdsample.svg')
+    print anSVG.getAllElements()
+    for element in anSVG.getAllElements():
+        if isinstance(element, TextContent)==False:
+            print element.get_id()
+    
+    for element in anSVG.getAllElementsOfHirarchy():
+         if isinstance(element, TextContent)==False:
+            print element.get_id()
+    
+    for element in anSVG.getElementsByType(Rect):
+        print element.get_id()
+        print element.getAttributes()
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
