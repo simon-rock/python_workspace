f571b21cf007ce83a98c7aeee0646398bf0c9448
simon-rock <simon29rock@gmail.com>
2014-01-13 17:26:32 +0800

    add load module example

---
+++ src/load_module.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#coding: utf-8
+#encoding: utf-8
+
+import os, sys, logging, getopt, simodule
+
+if __name__ == '__main__':
+    print simodule.GLOBAL_VAR_1
---
+++ src/simodule.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#coding: utf-8
+#encoding: utf-8
+
+GLOBAL_VAR_1 = '1.0.0.4'
+GLOBAL_VAR_2 = '1.0.0.6'
